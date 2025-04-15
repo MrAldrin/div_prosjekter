362576a4f8e9db066ee6d280bffb0561a633c5b4
hsa <hemingsa@math.uio.no>
2025-04-15 16:43:32 +0200

    improve runtime and logging

--- airplane_prices/web_scraper.py
+++ airplane_prices/web_scraper.py
@@ -13,6 +13,13 @@ import sys
 import pandas as pd
 import polars as pl
 import datetime as dt
+from concurrent.futures import ThreadPoolExecutor
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
 
 
 def get_prices(departure: str, arrival: str, date: str, print_out=False):
@@ -23,6 +30,7 @@ def get_prices(departure: str, arrival: str, date: str, print_out=False):
         arrival (str): example "AMS"
         date (str): example format "2025-06-04".
     """
+    logging.info(f"Starting to scrape {departure}-{arrival} on {date}")
 
     url = f"https://www.kayak.no/flights/{departure}-{arrival}/{date}?fs=fdDir=true;stops=~0&ucs=1a6g8dc&sort=price_a"
     if print_out:
@@ -32,12 +40,18 @@ def get_prices(departure: str, arrival: str, date: str, print_out=False):
     options.add_argument("--headless")
     # driver = webdriver.Chrome(options=chrome_options)
     driver = webdriver.Firefox(options=options)
-    driver.get(url)
 
-    sleep_time = random.uniform(a=2, b=3)
-    time.sleep(sleep_time)
+    try:
+        driver.get(url)
 
-    content = driver.page_source
+        sleep_time = 1.5
+        logging.info(f"Sleeping for {sleep_time:.2f} to wait for the page to load")
+        time.sleep(sleep_time)
+
+        content = driver.page_source
+    except Exception as e:
+        logging.error(f"Error while fetching the page: {e}. Returning empy df")
+        return pl.DataFrame()
     driver.quit()
 
     soup = BeautifulSoup(content, features="html.parser")
@@ -48,6 +62,9 @@ def get_prices(departure: str, arrival: str, date: str, print_out=False):
             "class": "yuAt yuAt-pres-rounded yuAt-mod-box-shadow yuAt-mod-responsive-margins"
         },
     )
+
+    if not results:
+        logging.warning(f"No results found for {departure} to {arrival} on {date}")
 
     flight_data = []
     for result in results:
@@ -85,30 +102,46 @@ def get_prices(departure: str, arrival: str, date: str, print_out=False):
                 "landing": landing,
             }
         )
+
+    logging.info(f"Scraped {len(flight_data)} flights {departure}-{arrival} on {date}")
     df = pl.DataFrame(flight_data)
     return df
 
 
-def get_prices_wrapper(
-    departures: list[str], arrivals: list[str], dates: list[str]
-) -> pl.DataFrame:
-    all_dfs = []
-    for dep in departures:
-        for dest in arrivals:
-            for date in dates:
-                df = get_prices(departure=dep, arrival=dest, date=date)
-                all_dfs.append(df)
+def get_prices_wrapper(departures, arrivals, dates):
+    logging.info("Starting to scrape multiple routes and dates")
+    futures = []
+    with ThreadPoolExecutor() as executor:
+        for dep in departures:
+            for dest in arrivals:
+                for date in dates:
+                    # Introduce a small random delay
+                    delay = random.uniform(0, 4.0)  # Delay between 0.5 and 2 seconds
+                    logging.info(f"Delaying task submission for {delay:.2f} seconds")
+                    time.sleep(delay)
 
-    return pl.concat(all_dfs)
+                    # Submit the task
+                    futures.append(executor.submit(get_prices, dep, dest, date))
+
+        # Collect results
+        results = [future.result() for future in futures]
+    logging.info("Finished scraping all routes and dates")
+    return pl.concat(results)
 
 
 # %%
 dep = ["OSL"]
 dests = ["HAM", "DUS", "AMS"]
 dates = ["2025-06-01", "2025-06-03"]
+
 df = get_prices_wrapper(departures=dep, arrivals=dests, dates=dates)
 
 now = dt.datetime.now().strftime("%Y-%m-%d_%H-%M")
-filename = f"flights_{dep[0]}_{now}.csv"
-df.write_csv(filename)
+filename = f"results/flights_{dep[0]}_{now}.csv"
+try:
+    df.write_csv(filename)
+    logging.info(f"Data successfully written to {filename}")
+except Exception as e:
+    logging.error(f"Failed to write CSV file: {e}")
+logging.info("Scraping process completed successfully")
 # %%
